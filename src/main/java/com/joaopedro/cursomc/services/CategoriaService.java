3b0aad87a148a60ac7e5b065458e15f1c7816cca
Joao Pedro <joaopedro_gon@outlook.com>
2020-08-04 12:01:54 -0300

    Estado e cidade

--- src/main/java/com/joaopedro/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaopedro/cursomc/services/CategoriaService.java
@@ -9,20 +9,19 @@ import com.joaopedro.cursomc.domain.Categoria;
 import com.joaopedro.cursomc.repositories.CategoriaRepository;
 import com.joaopedro.cursomc.services.exceptions.ObjectNotFoundException;
 
-@Service	
+@Service
 public class CategoriaService {
+	
 
 	@Autowired
 	private CategoriaRepository repo;
 	
+
 	public Categoria find(Integer id) {
-		 Optional<Categoria> obj = repo.findById(id);
-		 if(obj == null) {
-			 throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					 +", Tipo: " + Categoria.class.getName());
-		 }
-		return obj.orElse(null); 
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " 
+		+ Categoria.class.getName()));
 	}
-	
-	
 }
--- src/main/java/com/joaopedro/cursomc/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/joaopedro/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -1,14 +1,15 @@
 package com.joaopedro.cursomc.services.exceptions;
 
-public class ObjectNotFoundException extends RuntimeException{
+public class ObjectNotFoundException extends RuntimeException {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public ObjectNotFoundException(String msg) {
 		super(msg);
 	}
+
 	public ObjectNotFoundException(String msg, Throwable cause) {
 		super(msg, cause);
 	}
-	
 
 }
